f43bece6d191ad52166bc2adbbaca2dc0372d2ab
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-07-26 00:04:21 +0500

    done

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const searchProfile = () => {
                     <h6 class="card-title text-center text-body-tertiary mb-4">${data.login}</h6>
                     <p>${data.bio}</p>
                         <p class="card-text text-center mb-4"><span class="me-4"><i class="fa-solid fa-plus"></i> ${data.followers}  followers</span><i class="fa-regular fa-user"></i> ${data.following} following</p>
-                        <button type="button" class="btn btn-success text-white">Follow</button>
+                        <button onclick="${data.html_url}" type="button" class="btn btn-success text-white">Follow</button>
                     </div>
                     </div>
             </div>
