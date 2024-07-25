2f42385657877cd2abcaec353f9cd098cefcd6ac
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-07-25 23:58:06 +0500

    done

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ const searchProfile = () => {
         console.log(data);
             main.innerHTML = `
             <div class="col-md-3 d-flex justify-content-center align-items-center m-auto">
-                    <div class="card" style="width: 18rem;">
-                    <img src="images/frlIf.png" class="card-img-top size" alt="...">
+                    <div class="card">
+                    <img src="${data.avatar_url}" class="card-img-top size " alt="...">
                     <div class="card-body d-flex justify-content-center align-items-center m-auto flex-column">
-                        <h6 class="card-title text-center text-body-tertiary mb-4">${data.login}</h6>
-                        <p class="card-text text-center mb-4"><span class="me-4"><i class="fa-solid fa-plus"></i> 0 followers</span><i class="fa-regular fa-user"></i> 0 following</p>
+                    <h6 class="card-title text-center text-body-tertiary mb-4">${data.login}</h6>
+                    <p>${data.bio}</p>
+                        <p class="card-text text-center mb-4"><span class="me-4"><i class="fa-solid fa-plus"></i> ${data.followers}  followers</span><i class="fa-regular fa-user"></i> ${data.following} following</p>
                         <button type="button" class="btn btn-success text-white">Follow</button>
                     </div>
                     </div>
