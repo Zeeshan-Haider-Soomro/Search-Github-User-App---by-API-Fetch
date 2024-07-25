4cd302a49c5f5188e1d6a9c48ddbd345a62d17aa
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-07-25 23:50:34 +0500

    done'

--- app.js
+++ app.js
@@ -3,23 +3,19 @@ let userInput = document.getElementById('userInput');
 
 const searchProfile = () => {
     // fetch('https://api.github.com/users/$')
-    // fetch('https://api.github.com/users/$')
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch(`https://api.github.com/users/${userInput.value.toLowerCase()}`)
+    // fetch('https://jsonplaceholder.typicode.com/users')
     .then((res)=>{
         return res.json();
     })
     .then((data)=>{
         console.log(data);
-        let filteredData = data.filter((val)=>{
-            return val.username.toLowerCase() == userInput.value.toLowerCase();
-        })
-        filteredData.forEach((value)=> {
             main.innerHTML = `
             <div class="col-md-3 d-flex justify-content-center align-items-center m-auto">
                     <div class="card" style="width: 18rem;">
                     <img src="images/frlIf.png" class="card-img-top size" alt="...">
                     <div class="card-body d-flex justify-content-center align-items-center m-auto flex-column">
-                        <h6 class="card-title text-center text-body-tertiary mb-4">${value.username}</h6>
+                        <h6 class="card-title text-center text-body-tertiary mb-4">${data.login}</h6>
                         <p class="card-text text-center mb-4"><span class="me-4"><i class="fa-solid fa-plus"></i> 0 followers</span><i class="fa-regular fa-user"></i> 0 following</p>
                         <button type="button" class="btn btn-success text-white">Follow</button>
                     </div>
@@ -27,7 +23,7 @@ const searchProfile = () => {
             </div>
             `
         })
-    })
+
     .catch((error)=>{
         console.log(error);
     })
